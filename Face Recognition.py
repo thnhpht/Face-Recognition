61fe3ce510879b1dbfe645b969c28bc086416647
thnhpht <thnhpht1509@gmail.com>
2023-02-23 11:40:10 +0700

    update

--- Face Recognition.py
+++ Face Recognition.py
@@ -1,24 +1,55 @@
 import cv2
-from simple_facerec import SimpleFacerec
+import face_recognition
+import os
+import numpy as np
 
-# Load Camera
+path = 'Images'
+images = []
+classNames = []
+myList = os.listdir(path)
+print(myList)
+for cl in myList:
+    curImg = cv2.imread(f'{path}/{cl}')
+    images.append(curImg)
+    classNames.append(os.path.splitext(cl)[0])
+print(classNames)
+
+def findEncodings(images):
+    encodeList = []
+    for img in images:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        encode = face_recognition.face_encodings(img)[0]
+        encodeList.append(encode)
+    return encodeList
+
+encodeListKnown = findEncodings(images)
+print('Encoding Complete')
+
 cap = cv2.VideoCapture(2)
 
 while True:
     ret, frame = cap.read()
+    imgS = cv2.resize(frame, (0, 0), None, 0.25, 0.25)
+    imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    # Detect Faces
-    face_locations, face_names = sfr.detect_known_faces(frame)
+    facesCurFrame = face_recognition.face_locations(imgS)
+    encodesCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
 
-    for face_loc, name in zip(face_locations, face_names):
-        y1, x2, y2, x1 = face_loc[0], face_loc[1], face_loc[2], face_loc[3]
+    for encodeFace, faceLoc in zip(encodesCurFrame, facesCurFrame):
+        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+        print(faceDis)
+        matchIndex = np.argmin(faceDis)
 
-        # Show name and rectangle
-        cv2.putText(frame, name, (x1, y1 - 10), cv2.FONT_HERSHEY_DUPLEX, 1, (0, 0, 200), 2)
-        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 200), 4)
+        if matches[matchIndex]:
+            name = classNames[matchIndex].upper()
+            print(name)
+            y1, x2, y2, x1 = faceLoc 
+            y1, x2, y2, x1 =  y1*4, x2*4, y2*4, x1*4 
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+            cv2.rectangle(frame, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+            cv2.putText(frame, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255), 2)
 
-    cv2.imshow("Webcam", frame)
-
-    key = cv2.waitKey(1)
-    if key == 27:
-        break
+    
+    cv2.imshow('Webcam', frame)
+    cv2.waitKey(1)
